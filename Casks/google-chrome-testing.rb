a8c9f7f1fd21d494c3376420df6209116e83bc45
Vasyl Zuziak <zvasylvv@gmail.com>
2026-05-21 16:04:09 +0200

    fix: try to address sign issue

--- Casks/google-chrome-testing.rb
+++ Casks/google-chrome-testing.rb
@@ -1,7 +1,7 @@
 cask "google-chrome-testing" do
   arch arm: "arm64", intel: "x64"
 
-  version "148.0.7778.167"
+  version "149.0.7779.0"
   sha256 :no_check
 
   url "https://storage.googleapis.com/chrome-for-testing-public/#{version}/mac-#{arch}/chrome-mac-#{arch}.zip",
@@ -26,8 +26,8 @@ cask "google-chrome-testing" do
   preflight do
     cask_file_dir = Pathname.new(__FILE__).dirname
 
-    FileUtils.cp(cask_file_dir/"../Resources/google-chrome-for-testing.icns",
-                 "#{staged_path}/chrome-mac-#{arch}/Google Chrome for Testing.app/Contents/Resources/app.icns")
+    # FileUtils.cp(cask_file_dir/"../Resources/google-chrome-for-testing.icns",
+                 # "#{staged_path}/chrome-mac-#{arch}/Google Chrome for Testing.app/Contents/Resources/app.icns")
   end
 
   zap trash: [
