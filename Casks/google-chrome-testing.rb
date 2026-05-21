d08ac8f9e2e52ee809671535896e23849820b1d0
Vasyl Zuziak <zvasylvv@gmail.com>
2026-05-21 15:30:14 +0200

    feat+chore: add chrome + some service stuff

---
+++ Casks/google-chrome-testing.rb
@@ -0,0 +1,44 @@
+cask "google-chrome-testing" do
+  arch arm: "arm64", intel: "x64"
+
+  version "148.0.7778.167"
+  sha256 :no_check
+
+  url "https://storage.googleapis.com/chrome-for-testing-public/#{version}/mac-#{arch}/chrome-mac-#{arch}.zip",
+      verified: "storage.googleapis.com/chrome-for-testing-public/"
+  name "Google Chrome for Testing"
+  desc "Official Chrome build for testing automation"
+  homepage "https://googlechromelabs.github.io/chrome-for-testing/"
+
+  livecheck do
+    url "https://googlechromelabs.github.io/chrome-for-testing/last-known-good-versions-with-downloads.json"
+    strategy :page_match do |page|
+      data = JSON.parse(page)
+      data.dig("channels", "Stable", "version")
+    end
+  end
+
+  auto_updates false
+  depends_on macos: :big_sur
+
+  app "chrome-mac-#{arch}/Google Chrome for Testing.app", target: "Google Chrome for Testing.app"
+
+  preflight do
+    cask_file_dir = Pathname.new(__FILE__).dirname
+
+    FileUtils.cp(cask_file_dir/"../Resources/google-chrome-for-testing.icns",
+                 "#{staged_path}/chrome-mac-#{arch}/Google Chrome for Testing.app/Contents/Resources/app.icns")
+  end
+
+  zap trash: [
+        "~/Library/Application Support/Google/Chrome for Testing",
+        "~/Library/Caches/com.google.ChromeTesting",
+        "~/Library/Preferences/com.google.ChromeTesting.plist",
+        "~/Library/Saved Application State/com.google.ChromeTesting.savedState",
+      ],
+      rmdir: [
+        "~/Library/Application Support/Google/Chrome for Testing",
+        "~/Library/Caches/Google",
+        "~/Library/Google",
+      ]
+end
