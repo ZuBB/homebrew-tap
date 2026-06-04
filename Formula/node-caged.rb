d98e015caf5f264914926b992d9859dae13816d5
Vasyl Zuziak <zvasylvv@gmail.com>
2026-06-04 16:24:49 +0200

    Add node-caged formula

---
+++ Formula/node-caged.rb
@@ -0,0 +1,38 @@
+class NodeCaged < Formula
+  desc "Node.js with V8 pointer compression enabled"
+  homepage "https://github.com/platformatic/node-caged"
+  url "https://github.com/ZuBB/node-caged-bin/releases/download/v26.3.0-caged/node-v26.3.0-darwin-arm64.tar.gz"
+  sha256 "6dd25f453cf55538ef5768e63d8fccdc17dcb842a893c425650226a1d2117281"
+  license "MIT"
+
+  livecheck do
+    url "https://github.com/ZuBB/node-caged-bin/releases"
+    regex(/^v?(\d+(?:\.\d+)+)-caged$/i)
+  end
+
+  depends_on arch: :arm64
+  depends_on :macos
+
+  conflicts_with "node", because: "node-caged installs node, npm, and npx"
+
+  def install
+    prefix.install Dir["*"]
+  end
+
+  test do
+    assert_equal "v26.3.0", shell_output("#{bin}/node --version").strip
+    assert_equal "darwin-arm64", shell_output("#{bin}/node -p \"process.platform + '-' + process.arch\"").strip
+
+    pointer_config = shell_output("#{bin}/node -p \"[
+      process.config.variables.v8_enable_pointer_compression,
+      process.config.variables.v8_enable_31bit_smis_on_64bit_arch,
+      process.config.variables.v8_enable_external_code_space
+    ].join(',')\"").strip
+    assert_equal "1,1,1", pointer_config
+
+    heap_limit = shell_output("#{bin}/node -p \"require('v8').getHeapStatistics().heap_size_limit\"").to_i
+    assert_operator heap_limit, :<=, 4 * 1024 * 1024 * 1024
+
+    assert_match "npm", shell_output("#{bin}/npm --version")
+  end
+end
